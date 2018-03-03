a3ece8d17fb1ba8ab5b788d1164d7b7e333185d4
anshul_gupta <ag19rishu@gmail.com>
2018-03-04 02:17:40 +0530

    Initial commit

--- src/color.cpp
+++ src/color.cpp
@@ -3,4 +3,6 @@
 const color_t COLOR_RED = { 236, 100, 75 };
 const color_t COLOR_GREEN = { 135, 211, 124 };
 const color_t COLOR_BLACK = { 52, 73, 94 };
-const color_t COLOR_BACKGROUND = { 242, 241, 239 };
+const color_t COLOR_BLUE = { 0, 135, 220 };
+const color_t COLOR_BACKGROUND = { 185, 245, 246 };
+// const color_t COLOR_BACKGROUND = { 242, 241, 239 };
--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,17 @@ GLFWwindow *window;
 * Customizable functions *
 **************************/
 
-Ball ball1;
+Ball boat;
+Ball rock;
+Ball water;
 
 float screen_zoom = 1, screen_center_x = 0, screen_center_y = 0;
 float camera_rotation_angle = 0;
+glm::vec3 eye (5, 0.09, 0);
+glm::vec3 target (0, 0, 0);
+glm::vec3 up (0, 1, 0);
+int view = 2;
+double cursor_x, cursor_y;
 
 Timer t60(1.0 / 60);
 
@@ -30,12 +37,12 @@ void draw() {
     glUseProgram (programID);
 
     // Eye - Location of camera. Don't change unless you are sure!!
-    glm::vec3 eye ( 5*cos(camera_rotation_angle*M_PI/180.0f), 0, 5*sin(camera_rotation_angle*M_PI/180.0f) );
+    // glm::vec3 eye ( 5*cos(camera_rotation_angle*M_PI/180.0f), 0, 5*sin(camera_rotation_angle*M_PI/180.0f) );
+    // eye = camera_position;
     // Target - Where is the camera looking at.  Don't change unless you are sure!!
-    glm::vec3 target (0, 0, 0);
+    // target = camera_target;
     // Up - Up vector defines tilt of camera.  Don't change unless you are sure!!
-    glm::vec3 up (0, 1, 0);
-
+    // glm::vec3 up (0, 1, 0);
     // Compute Camera matrix (view)
     Matrices.view = glm::lookAt( eye, target, up ); // Rotating Camera for 3D
     // Don't change unless you are sure!!
@@ -51,20 +58,70 @@ void draw() {
     glm::mat4 MVP;  // MVP = Projection * View * Model
 
     // Scene render
-    ball1.draw(VP);
+    water.draw(VP);
+    boat.draw(VP);
+    rock.draw(VP);
 }
 
 void tick_input(GLFWwindow *window) {
+    int up  = glfwGetKey(window, GLFW_KEY_UP);
+    int down  = glfwGetKey(window, GLFW_KEY_DOWN);
     int left  = glfwGetKey(window, GLFW_KEY_LEFT);
     int right = glfwGetKey(window, GLFW_KEY_RIGHT);
+    int c = glfwGetKey(window, GLFW_KEY_C);
+    glfwGetCursorPos(window, &cursor_x, &cursor_y);
+    int mouse_left = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+
     if (left) {
-        // Do something
+      boat.rotation += 1;
+    }
+    if (right) {
+      boat.rotation -= 1;
+    }
+    if (up) {
+      boat.position.x -= 0.05*cos(boat.rotation * M_PI / 180.0f);
+      boat.position.z += 0.05*sin(boat.rotation * M_PI / 180.0f);
+    }
+    if (down) {
+      boat.position.x += 0.05*cos(boat.rotation * M_PI / 180.0f);
+      boat.position.z -= 0.05*sin(boat.rotation * M_PI / 180.0f);
+    }
+    if (mouse_left) {
+      // cout<<"mouse_left\n";
+    }
+    if (c) {
+      view = (view + 1) % 4;
     }
 }
-
+unsigned long long int j = 0;
 void tick_elements() {
-    ball1.tick();
+    boat.tick();
+    // water.tick();
+    boat.position.y += 0.02*sin((++j)/7);
     camera_rotation_angle += 1;
+    if (view == 0) {                                                //follow_view
+      target.x = boat.position.x;
+      target.z = boat.position.z;
+      eye.x = boat.position.x + 5*cos(boat.rotation * M_PI / 180.0f);
+      eye.z = boat.position.z - 5*sin(boat.rotation * M_PI / 180.0f);
+    }
+    if (view == 1) {                                                //boat_view
+      eye = boat.position - glm::vec3(0.50, 0, 0);
+      target = boat.position - glm::vec3 (2, 0, 0);
+    }
+    if (view == 2) {                                                //top_view
+      eye = boat.position + glm::vec3(0, 5, 0);
+      target = boat.position;
+      up.x = -cos(boat.rotation * M_PI / 180.0f);
+      up.z = sin(boat.rotation * M_PI / 180.0f);
+    }
+    if (view == 3) {                                                //tower_view
+      eye = glm::vec3 (0, 10, 10);
+      target.x = boat.position.x;
+      target.z = boat.position.z;
+    }
+    // cout<<cursor_x<<" "<<cursor_y<<endl;
+
 }
 
 /* Initialize the OpenGL rendering properties */
@@ -73,7 +130,9 @@ void initGL(GLFWwindow *window, int width, int height) {
     /* Objects should be created before any other gl function and shaders */
     // Create the models
 
-    ball1       = Ball(0, 0, COLOR_RED);
+    boat       = Ball(1, -1.7, 0, 0.5, 0.5, 0.5, COLOR_RED);
+    rock       = Ball(2, -1.7, 0, 0.5, 0.5, 0.5, COLOR_BLACK);
+    water       = Ball(0, -3, 0, 1000, 1, 1000, COLOR_BLUE);
 
     // Create and compile our GLSL program from the shaders
     programID = LoadShaders("Sample_GL.vert", "Sample_GL.frag");
@@ -99,8 +158,8 @@ void initGL(GLFWwindow *window, int width, int height) {
 
 int main(int argc, char **argv) {
     srand(time(0));
-    int width  = 600;
-    int height = 600;
+    int width  = 1000;
+    int height = 1000;
 
     window = initGLFW(width, height);
 
@@ -138,5 +197,6 @@ void reset_screen() {
     float bottom = screen_center_y - 4 / screen_zoom;
     float left   = screen_center_x - 4 / screen_zoom;
     float right  = screen_center_x + 4 / screen_zoom;
-    Matrices.projection = glm::ortho(left, right, bottom, top, 0.1f, 500.0f);
+    // Matrices.projection = glm::ortho(left, right, bottom, top, 0.1f, 500.0f);
+    Matrices.projection = glm::perspective(2.0, 1.0, 0.01, 100.0);
 }
